d7387a723918b826fdf6459723d30c96624af0a8
Saman Abbasi <samanabbasi.ir@gmail.com>
2025-05-14 09:28:13 +0330

    fix: correct import statement for users.controller in users.routes.js

--- src/backend/apis/users/users.routes.js
+++ src/backend/apis/users/users.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { createUser, getUsers } from './users.controller,js'
+import { createUser, getUsers } from './users.controller.js'
 
 const router = Router()
 
