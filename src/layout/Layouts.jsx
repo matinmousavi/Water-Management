cb83e1f4619dc39e57e22a9ea7f3fad6e5a17c80
Erfanfooladii <arfannnfooladi@gmail.com>
2025-05-14 10:34:08 +0330

    revert; remove zeroWidthTriggerStyle

--- src/layout/Layouts.jsx
+++ src/layout/Layouts.jsx
@@ -22,20 +22,10 @@ const Layouts = () => {
 	//const { isAdmin } = useUser()
 
 	return (
-		<Layout className={styles.layout}>
+		<Layout>
 			<Sider
 				breakpoint='lg'
 				collapsedWidth='0'
-				zeroWidthTriggerStyle={{
-					backgroundColor: 'transparent',
-					top: 30,
-					right: 20,
-					borderRadius: 0,
-					color: '#000',
-					width: 30,
-					height: 30,
-					fontSize: 17,
-				}}
 				zeroWidthTriggerStyle={{
 					backgroundColor: 'transparent',
 					top: 30,
@@ -67,10 +57,8 @@ const Layouts = () => {
 				<Menu theme='dark' mode='inline' defaultSelectedKeys={['1']} items={/* isAdmin ? itemsUser : */ itemsAdmin} className={style.menu} />
 			</Sider>
 			<Layout>
-				<Content className={styles.content}>
-					<Card>
-						<Outlet />
-					</Card>
+				<Content className={style.content}>
+					<Outlet />
 				</Content>
 			</Layout>
 		</Layout>
