46ca104627c99efe505fe0de1f91ba43d8811424
Saman Abbasi <samanabbasi.ir@gmail.com>
2025-05-16 11:08:25 +0330

    feat: add admin authorization for profile picture upload route

--- src/backend/apis/upload/upload.routes.js
+++ src/backend/apis/upload/upload.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { uploadProfilePicture } from './upload.controller.js'
+import { isAdmin } from '../../middlewares/auth.js'
 
 const router = Router()
 
 router.post('/profile/picture', uploadProfilePicture)
-router.post('/profile/picture/:userId', uploadProfilePicture)
+router.post('/profile/picture/:userId', isAdmin, uploadProfilePicture)
 
 export default router
