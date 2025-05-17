2e48a91a1cfdeaeadaa7175315448f0660654b2f
Erfanfooladii <arfannnfooladi@gmail.com>
2025-05-17 14:02:26 +0330

    feat: add delete confirmation modal

--- src/pages/shared/Profile/Profile.jsx
+++ src/pages/shared/Profile/Profile.jsx
@@ -6,8 +6,10 @@ import Loading from '../../../components/Loading/Loading'
 import { useParams } from 'react-router'
 import ProfileImageCard from './components/ProfileImageCard/ProfileImageCard'
 import DeleteUserCard from './components/DeleteUserCard/DeleteUserCard'
+import { useUser } from '../../../contexts/UserContext'
 
 const Profile = () => {
+	const { isAdmin } = useUser()
 	const profileApi = useAPI()
 	const { data, isLoading } = profileApi
 	const { userId } = useParams()
@@ -22,7 +24,7 @@ const Profile = () => {
 					<h1 className={styles.title}>پروفایل - متین موسوی</h1>
 					<ProfileImageCard initialSrc={data?.user?.profilePicture?.url} />
 					<ContactInfoCard userData={data.user} profileApi={profileApi} />
-					<DeleteUserCard />
+					{isAdmin && <DeleteUserCard />}
 				</>
 			)}
 		</Flex>
