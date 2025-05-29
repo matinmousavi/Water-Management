0f9e2021abc2cc65ce5dd2e21db4c39a93535b95
Saman Abbasi <samanabbasi.ir@gmail.com>
2025-05-29 22:05:26 +0330

    refactor: remove unused react-scan import and initialization for cleaner code

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { scan } from 'react-scan'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -8,10 +7,6 @@ import faIR from 'antd/locale/fa_IR'
 
 import App from './App.jsx'
 import './index.css'
-
-scan({
-	enabled: true,
-})
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
