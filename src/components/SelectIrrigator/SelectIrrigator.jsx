6a08ec8fea4cfc211f80f025e5229150829bf731
ErfanFooladii <arfannnfooladi@gmail.com>
2025-05-30 14:47:10 +0330

    feat: Add Select for irrigator role

---
+++ src/components/SelectIrrigator/SelectIrrigator.jsx
@@ -0,0 +1,44 @@
+import { Select } from 'antd'
+import { useEffect } from 'react'
+import useAPI from '../../hooks/useAPI'
+
+const SelectIrrigator = ({ value, onChange, defaultOpen }) => {
+	const api = useAPI()
+
+	useEffect(() => {
+		api.get('users', { role: 'irrigator' })
+	}, [])
+
+	const handleChange = selectedId => {
+		if (!selectedId) {
+			onChange(null)
+			return
+		}
+		const selected = api.data?.users?.find(user => user._id === selectedId)
+		onChange?.({
+			_id: selectedId,
+			firstName: selected?.firstName,
+			lastName: selected?.lastName,
+		})
+	}
+
+	return (
+		<Select
+			showSearch
+			placeholder='میراب را انتخاب کنید'
+			loading={api.isLoading}
+			value={value?._id}
+			onChange={handleChange}
+			allowClear
+			defaultOpen={defaultOpen}
+			style={{ width: '100%' }}
+			filterOption={(input, option) => (option?.label ?? '').toLowerCase().includes(input.toLowerCase())}
+			options={api.data?.users?.map(user => ({
+				value: user._id,
+				label: `${user.firstName} ${user.lastName}`,
+			}))}
+		/>
+	)
+}
+
+export default SelectIrrigator
