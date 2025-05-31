9613c7d0544b9d4235b5ed8bae3541009239990e
Saman Abbasi <samanabbasi.ir@gmail.com>
2025-05-31 22:53:37 +0330

    feat: add jalaliday dependency and improve mobile responsiveness in Layouts component

--- src/routes/LayoutRoutes.jsx
+++ src/routes/LayoutRoutes.jsx
@@ -11,34 +11,30 @@ import Wells from '../pages/admin/Wells/Wells'
 import Well from '../pages/shared/Well/Well'
 
 const LayoutRoutes = () => {
-	const { isLogin, isAdmin, isIrrigator } = useUser()
+	const { isAdmin, isIrrigator } = useUser()
 
 	return (
 		<Routes>
-			{!isLogin ? (
-				<Route path='/*' element={<Login />} />
-			) : (
-				<Route element={<Layouts />}>
-					<Route index element={<Dashboard />} />
-					<Route path='/profile' element={<Profile />} />
+			<Route element={<Layouts />}>
+				<Route index element={<Dashboard />} />
+				<Route path='/profile' element={<Profile />} />
 
-					{isAdmin && (
-						<>
-							<Route path='/users' element={<Users />} />
-							<Route path='/users/:userId' element={<Profile />} />
-							<Route path='/wells/:wellId' element={<Well />} />
-							<Route path='/lands' element={<Lands />} />
-							<Route path='/lands/:landId' element={<Land />} />
-						</>
-					)}
+				{isAdmin && (
+					<>
+						<Route path='/users' element={<Users />} />
+						<Route path='/users/:userId' element={<Profile />} />
+						<Route path='/wells/:wellId' element={<Well />} />
+						<Route path='/lands' element={<Lands />} />
+						<Route path='/lands/:landId' element={<Land />} />
+					</>
+				)}
 
-					{(isIrrigator || isAdmin) && (
-						<>
-							<Route path='/wells' element={<Wells />} />
-						</>
-					)}
-				</Route>
-			)}
+				{(isIrrigator || isAdmin) && (
+					<>
+						<Route path='/wells' element={<Wells />} />
+					</>
+				)}
+			</Route>
 		</Routes>
 	)
 }
