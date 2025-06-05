8c9eb8c29e41d6612d352fcf979218106b2327bd
mahdieh <madidgn82@gmail.com>
2025-06-05 12:44:32 +0330

    feat: enhance Notification Settings with input fields and save functionality

--- src/pages/admin/NotificationSettings/NotificationSettings.jsx
+++ src/pages/admin/NotificationSettings/NotificationSettings.jsx
@@ -1,7 +1,8 @@
-import { Flex, Card, Typography } from 'antd'
+import { Flex, Card, Typography, Input, Button, Row, Col } from 'antd'
 import BreadCrumbs from '../../../components/BreadCrumbs/BreadCrumbs'
 
-const { Title } = Typography
+const { Title, Text } = Typography
+const { TextArea } = Input
 
 const NotificationSettings = () => {
 	return (
@@ -9,10 +10,57 @@ const NotificationSettings = () => {
 			<BreadCrumbs />
 			<Flex vertical gap={16}>
 				<Card>
-					<Title className='text-h2'>اطلاع رسانی OTP</Title>
+					<Flex vertical gap={16}>
+						<Title level={2} className='text-h2'>
+							اطلاع رسانی OTP
+						</Title>
+						<Text>پیامک حاوی کد تأیید هنگام ورود کاربر به سیستم ارسال می‌شود.</Text>
+						<TextArea rows={4} placeholder='متن پیامک را وارد کنید' />
+						<Flex justify='end'>
+							<Button type='primary'>ذخیره</Button>
+						</Flex>
+					</Flex>
 				</Card>
 				<Card>
-					<Title className='text-h2'>اطلاع رسانی آبرسانی</Title>
+					<Flex vertical gap={20}>
+						<Title level={2} className='text-h2'>
+							اطلاع رسانی آبرسانی
+						</Title>
+						<Text>پیامک حاوی اطلاعات آبرسانی به میرآب و مالک زمین ارسال می‌شود.</Text>
+						<Flex vertical justify='space-between'>
+							<Row gutter={16}>
+								<Col span={12}>
+									<Flex gap={8} vertical>
+										<Text>شروع آبرسانی برای میرآب</Text>
+										<TextArea rows={4} placeholder='متن پیامک را وارد کنید' />
+									</Flex>
+								</Col>
+								<Col span={12}>
+									<Flex gap={8} vertical>
+										<Text>پایان آبرسانی برای میرآب</Text>
+										<TextArea rows={4} placeholder='متن پیامک را وارد کنید' />
+									</Flex>
+								</Col>
+							</Row>
+							<Row gutter={16}>
+								<Col span={12}>
+									<Flex gap={8} vertical>
+										<Text>شروع آبرسانی برای مالک زمین</Text>
+										<TextArea rows={4} placeholder='متن پیامک را وارد کنید' />
+									</Flex>
+								</Col>
+								<Col span={12}>
+									<Flex gap={8} vertical>
+										<Text>پایان آبرسانی برای مالک زمین</Text>
+										<TextArea rows={4} placeholder='متن پیامک را وارد کنید' />
+									</Flex>
+								</Col>
+							</Row>
+						</Flex>
+						<Flex justify='end'>
+							<Button type='primary'>ذخیره</Button>
+						</Flex>
+					</Flex>
 				</Card>
 			</Flex>
 		</Flex>
