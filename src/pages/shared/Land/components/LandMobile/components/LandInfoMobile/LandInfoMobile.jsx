c8b593f999a12631cf2196014b51fcc85830429f
mahdieh <madidgn82@gmail.com>
2025-07-06 01:27:31 +0330

    fix LandInfoMobile component

--- src/pages/shared/Land/components/LandMobile/components/LandInfoMobile/LandInfoMobile.jsx
+++ src/pages/shared/Land/components/LandMobile/components/LandInfoMobile/LandInfoMobile.jsx
@@ -1,18 +1,14 @@
-import { Card, Flex, Table, Typography, Button, Drawer } from 'antd'
+import { Typography, Drawer } from 'antd'
 import moment from 'moment-jalaali'
 import styles from './LandInfoMobile.module.css'
-import iconClock from '../../../../../../../assets/icons/ClockCircleOutlined.svg'
-import iconLocation from '../../../../../../../assets/icons/EnvironmentOutlined.svg'
-import iconContacts from '../../../../../../../assets/icons/ContactsOutlined.svg'
-import iconPhone from '../../../../../../../assets/icons/PhoneOutlined.svg'
 import { useState, useEffect, useRef } from 'react'
 import TimeStartPickerSheet from './components/TimeStartPickerSheet/TimeStartPickerSheet'
 import TimeEndPickerSheet from './components/TimeEndPickerSheet/TimeEndPickerSheet'
 import EndNoticeDrawer from './components/EndNoticeDrawer/EndNoticeDrawer'
 import useAPI from '../../../../../../../hooks/useAPI'
 import { useParams } from 'react-router'
-import DescriptionModalCell from './components/DescriptionModalCell/DescriptionModalCell'
 import { useIrrigationTimer } from '../../../../../../../contexts/IrrigationTimerContext'
+import TableAndInfoMobile from './components/TableAndInfoMobile/TableAndInfoMobile'
 
 const { Text } = Typography
 
@@ -29,7 +25,6 @@ const LandInfoMobile = ({ data }) => {
 
 	const startY = useRef(0)
 	const { elapsedTime, isIrrigating, startIrrigation, stopIrrigation, landID } = useIrrigationTimer()
-	const isCurrentLandIrrigating = isIrrigating && logs.some(log => log.isOngoing && log.land?._id === landID)
 
 	useEffect(() => {
 		if (api.data?.land?.logs?.length && logs.length === 0) {
@@ -122,86 +117,18 @@ const LandInfoMobile = ({ data }) => {
 		setShowEndDrawer(false)
 	}
 
-	const listItems = [
-		{ icon: iconContacts, title: 'نام زمین', value: data?.title },
-		{ icon: iconPhone, title: 'شماره تماس', value: data?.owner?.mobile },
-		{ icon: iconLocation, title: 'آدرس زمین', value: data?.location },
-		{ icon: iconClock, title: 'زمان آبیاری بعدی', value: moment(data?.updatedAt).format('dddd jD jMMMM jYYYY') || '-' },
-		{ icon: iconClock, title: 'آخرین زمان آبیاری', value: moment(data?.createdAt).format('dddd jD jMMMM jYYYY') || '-' },
-	]
-
-	const columns = [
-		{
-			title: 'تاریخ',
-			dataIndex: 'startedAt',
-			key: 'date',
-			render: value => moment(value).format('dddd jD jMMMM jYYYY'),
-		},
-		{
-			title: 'ساعت شروع',
-			dataIndex: 'startedAt',
-			key: 'startTime',
-			render: value => (value ? moment(value).format('HH:mm') : '--'),
-		},
-		{
-			title: 'مدت زمان آبیاری',
-			key: 'duration',
-			render: (text, record) => {
-				if (record.isOngoing) return 'در حال آبیاری'
-				if (!record.duration) return '--'
-
-				const [h, m] = record.duration.split(':').map(Number)
-				return h === 0 ? `${m} دقیقه` : `${h} ساعت${m > 0 ? ` و ${m} دقیقه` : ''}`
-			},
-		},
-		{
-			title: 'توضیحات',
-			key: 'notes',
-			render: record => <DescriptionModalCell record={record} />,
-		},
-	]
-
 	return (
 		<div className={styles.container}>
-			<Flex gap={16} vertical>
-				<Card className={styles.card}>
-					<Flex vertical gap={8}>
-						{listItems.map((item, idx) => (
-							<Flex className={styles.itemCaar} key={idx} gap={10} align='center' justify='center'>
-								<Flex gap={8} className={styles.cardType}>
-									<img src={item.icon} alt='icon' />
-									<Text>{item.title}</Text>
-								</Flex>
-								<Flex className={styles.cardRole}>
-									<Text>{item.value}</Text>
-								</Flex>
-							</Flex>
-						))}
-					</Flex>
-				</Card>
-
-				<Card>
-					<Flex vertical gap={8}>
-						<Text>لاگ توزیع آب ({logs?.length})</Text>
-						<Table rowKey='_id' scroll={{ x: 'max-content' }} pagination={false} className={styles.table} dataSource={logs} columns={columns} />
-					</Flex>
-				</Card>
-			</Flex>
-
-			<div className={styles.footer}>
-				{isCurrentLandIrrigating ? (
-					<>
-						<Text className={`${styles.timerText} ${elapsedTime <= 900 ? styles.timerDanger : ''}`}>{formatTime(elapsedTime)}</Text>
-						<Button type='default' className={`${elapsedTime <= 900 ? styles.btnDanger : 'style-btn'}`} onClick={handleStop}>
-							پایان آبیاری
-						</Button>
-					</>
-				) : (
-					<Button type='primary' block onClick={() => setShowStartDrawer(true)}>
-						شروع آبیاری
-					</Button>
-				)}
-			</div>
+			<TableAndInfoMobile
+				data={data}
+				logs={logs}
+				elapsedTime={elapsedTime}
+				time={formatTime(elapsedTime)}
+				handleStop={handleStop}
+				setShowStartDrawer={setShowStartDrawer}
+				landID={landID}
+				isIrrigating={isIrrigating}
+			/>
 
 			<Drawer title={null} placement='bottom' height='auto' open={showStartDrawer} onClose={() => setShowStartDrawer(false)} closable={false}>
 				<div onTouchStart={handleTouchStart} onTouchMove={handleTouchMove}>
