8401bb2bd03afc65057b9c77aa773088f423fa0d
matinmousavi <matinmousavi5049@gmail.com>
2025-07-08 16:42:06 +0330

    feat: add Breadcrumbs component to Dashboard

--- src/pages/shared/Dashboard/Dashboard.jsx
+++ src/pages/shared/Dashboard/Dashboard.jsx
@@ -1,14 +1,18 @@
 import { Card, Typography } from 'antd'
+import Breadcrumbs from '../../../components/BreadCrumbs/BreadCrumbs'
 
 const { Title } = Typography
 
 const Dashboard = () => {
 	return (
+		<>
+		<Breadcrumbs />
 		<Card>
 			<Title level={1} className='text-h1'>
 				داشبورد
 			</Title>
 		</Card>
+		</>
 	)
 }
 
