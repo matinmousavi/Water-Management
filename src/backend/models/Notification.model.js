bc0d267992ea928dd0ec3e98a1bcb50c3586879c
Saman Abbasi <samanabbasi.ir@gmail.com>
2025-07-21 04:42:54 +0330

    feat: add notifications API and model for managing user notifications

---
+++ src/backend/models/Notification.model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+
+const notificationSchema = new mongoose.Schema(
+	{
+		recipientGroup: {
+			type: String,
+			enum: ['all', 'admins', 'irrigators', 'landOwners'],
+			required: true,
+		},
+		recipients: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+
+		message: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		medium: {
+			type: String,
+			enum: ['sms', 'email', 'push'],
+			default: 'sms',
+		},
+		sentBy: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		sentAt: {
+			type: Date,
+			default: Date.now,
+		},
+		meta: {
+			successCount: { type: Number, default: 0 },
+			failCount: { type: Number, default: 0 },
+		},
+	},
+	{ timestamps: true }
+)
+
+export default mongoose.model('Notification', notificationSchema)
