d96265091114e33838674aea65afc4bd50bab0ed
Erfanfooladii <arfannnfooladi@gmail.com>
2025-07-22 05:35:14 +0330

    refactor: remove unused Typography import from WellsList component

--- src/pages/shared/Well/components/WellsList/WellsList.jsx
+++ src/pages/shared/Well/components/WellsList/WellsList.jsx
@@ -1,10 +1,8 @@
-import { Button, Drawer, Flex, List, Typography } from 'antd'
+import { Button, Flex, List } from 'antd'
 import styles from './WellsList.module.css'
 import ModalMobile from '../../../../../components/ModalMobile/ModalMobile'
 
 const WellsList = ({ open, onClose, data, setData }) => {
-	const { Title } = Typography
-
 	return (
 		<ModalMobile open={open} isList title='انتخاب چاه' onClose={onClose} handleSubmit={setData}>
 			<Flex gap={8} vertical>
