aaea4bcc9415468e9ca537f3e50549fdc8e41c0c
Erfanfooladii <arfannnfooladi@gmail.com>
2025-09-01 16:16:28 +0330

    fix: hidden link fullname for mirab panel

--- src/pages/shared/Well/components/WellDesktopView/components/WellLogsCard/components/WellLogsTable/WellLogsTable.jsx
+++ src/pages/shared/Well/components/WellDesktopView/components/WellLogsCard/components/WellLogsTable/WellLogsTable.jsx
@@ -143,7 +143,16 @@ const WellLogsTable = ({ data, setLogs, wellStatus }) => {
 		{
 			title: 'مالک زمین',
 			width: 180,
-			render: (_, record) => (record.land?.owner ? <Link to={`/users/${record.land.owner._id}`}>{record.land.owner.fullName}</Link> : '--'),
+			render: (_, record) =>
+				record.land?.owner ? (
+					isAdmin ? (
+						<Link to={`/users/${record.land.owner._id}`}>{record.land.owner.fullName}</Link>
+					) : (
+						record.land.owner.fullName
+					)
+				) : (
+					'--'
+				),
 		},
 		{
 			title: 'توضیحات',
