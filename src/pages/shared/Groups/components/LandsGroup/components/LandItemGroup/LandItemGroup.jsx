3103b71c56805968f3c8750e7ffc546802481501
Saman Abbasi <samanabbasi.ir@gmail.com>
2025-09-02 09:54:55 +0330

    feat(ui):  enable progress bar in LandItemGroup

--- src/pages/shared/Groups/components/LandsGroup/components/LandItemGroup/LandItemGroup.jsx
+++ src/pages/shared/Groups/components/LandsGroup/components/LandItemGroup/LandItemGroup.jsx
@@ -12,7 +12,7 @@ import ProgressBar from '../../../../../../../components/ProgressBar/ProgressBar
 const LandItemGroup = ({ data }) => {
 	const { Text } = Typography
 
-	const sections = Math.floor(Math.random() * 4) + 2
+	const sections = 4
 	const progressValue = 40
 
 	const landData = [
@@ -57,7 +57,7 @@ const LandItemGroup = ({ data }) => {
 						<Flex className={styles.cardRole}>{item.content}</Flex>
 					</Flex>
 				))}
-				{/* <ProgressBar sections={sections} progressValue={progressValue} /> */}
+				<ProgressBar sections={sections} progressValue={progressValue} />
 			</Flex>
 		</Card>
 	)
